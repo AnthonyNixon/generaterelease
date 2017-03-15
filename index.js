581d867d5dc84b045f70164b9b0c517d45745fca
Anthony Nixon <ajn0592@gmail.com>
2017-03-14 22:48:35 -0500

    Add query variable
    Test the query variables being read.

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
  */
 exports.helloWorld = function helloWorld(req, res) {
   // Example input: {"message": "Hello!"}
-  if (req.body.message === undefined) {
+  if (req.query.message === undefined) {
     // This is an error case, as "message" is required.
     res.status(400).send('No message defined!');
   } else {
     // Everything is okay.
-    console.log(req.body.message);
-    res.status(200).send('Success: ' + req.body.message);
+    console.log(req.query.message);
+    res.status(200).send('Success: ' + req.query.message);
   }
 };
