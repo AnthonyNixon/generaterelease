f22e54096c6e76c23b76098c67fd33a90cab143f
Anthony Nixon <ajn0592@gmail.com>
2017-03-15 21:01:00 -0500

    fix tabs in files

--- generatefiles.py
+++ generatefiles.py
@@ -3,16 +3,19 @@ with open ("animals-list.txt", "a+") as animalsfile:
 animalsfile.close()
 
 processedLetters = []
+lettersToArray = {}
 
 for animal in animals:
     animal = animal.replace('\n','').replace('\r','')
     print animal
     firstLetter = animal[0].upper()
     if firstLetter not in processedLetters:
-        animalfile = open('animals-by-letter/' + firstLetter, "w+")
-        animalfile.write('')
-        animalfile.close()
+        lettersToArray[firstLetter] = []
         processedLetters.append(firstLetter)
-    animalfile = open('animals-by-letter/' + firstLetter, "a+")
-    animalfile.write(animal + "\t")
+    lettersToArray[firstLetter].append(animal)
+
+for letter in lettersToArray:
+
+    animalfile = open('animals-by-letter/' + letter, "w+")
+    animalfile.write("\t".join(lettersToArray[letter]))
     animalfile.close()
